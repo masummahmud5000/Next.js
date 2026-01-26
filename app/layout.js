b998c79270d5f70afe516ab1a2e81fff3669834a
Masum Mahmud <mdmasumpresent@gmail.com>
2026-01-26 12:38:50 +0600

    new

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import logoImg from '@/app/favicon.ico'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className='bg-linear-to-l from-blue-300 to-green-400'>
+      <body className='bg-linear-to-l from-blue-300 to-green-400 flex flex-col min-h-screen'>
         <nav className='sticky top-0 bg-red-500 h-16 flex items-center border-b-2 border-white pl-5 pr-3'>
           <h1 className='logoName text-white text-xl flex'><Image src={logoImg} alt='this is the Logo Image' height={30} className='bg-white mr-2 p-1 rounded-4xl animate-[spin_4s_linear_infinite]'/><span className='text-amber-300'>M</span>asum <span className='text-green-400 ml-1'>M</span>ahmud</h1>
           <ul className='grid grid-cols-2 ml-10 space-x-2 text-green-100 cursor-pointer'>
@@ -17,8 +17,12 @@ export default function RootLayout({ children }) {
             <Link href={'/login'} className='hover:text-2xl hover:text-white transition-all duration-300'>Login</Link>
           </ul>
         </nav>
-        {children}
-        <footer className='flex flex-col mt-30 gap-3 bg-black text-white h-40 items-center justify-center border-5 border-x border-b'>
+        
+        <main className='flex-1'>
+          {children}
+        </main>
+        
+        <footer className=' flex flex-col mt-30 gap-3 bg-black text-white h-40 items-center justify-center border-3 border-b rounded-3xl rounded-b'>
           <h1 className='text-white'>সৌজন্যে : মাসুম <h1 className='fa fa-heart text-red-600'/> মনিরের ফুফু </h1>
           <h1>© Copyright 2026</h1>
           <h1 className='text-green-600'>Admin : masum@gmail.com</h1>
