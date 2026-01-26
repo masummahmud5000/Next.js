88749099de10385470fe18786b874460aab1b0ca
Masum Mahmud <mdmasumpresent@gmail.com>
2026-01-26 11:45:27 +0600

    new

--- app/layout.js
+++ app/layout.js
@@ -8,10 +8,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className='bg-linear-to-l from-blue-300 to-green-400'>
-        <nav className='sticky top-0 bg-red-500 h-18 flex items-center border-b-2 border-white pl-10'>
-          <h1 className='logoName text-white text-3xl flex'><Image src={logoImg} alt='this is the Logo Image' height={40} className='bg-white mr-2 p-1 rounded-4xl animate-[spin_4s_linear_infinite]'/><span className='text-amber-300'>M</span>asum <span className='text-green-400 ml-2'>M</span>ahmud</h1>
-          <ul className='flex ml-100 space-x-12 text-xl text-green-100 cursor-pointer'>
-            <Link href={'/'} className='hover:text-2xl hover:text-white transition-all duration-300'>Home</Link>
+        <nav className='sticky top-0 bg-red-500 h-16 flex items-center border-b-2 border-white pl-5 pr-3'>
+          <h1 className='logoName text-white text-xl flex'><Image src={logoImg} alt='this is the Logo Image' height={30} className='bg-white mr-2 p-1 rounded-4xl animate-[spin_4s_linear_infinite]'/><span className='text-amber-300'>M</span>asum <span className='text-green-400 ml-1'>M</span>ahmud</h1>
+          <ul className='grid grid-cols-2 ml-10 space-x-2 text-green-100 cursor-pointer'>
+            <Link href={'/'} className='hover:text-xl hover:text-white transition-all duration-300'>Home</Link>
             <Link href={'/dash'} className='hover:text-2xl hover:text-white transition-all duration-300'>Dashboard</Link>
             <Link href={'/register'} className='hover:text-2xl hover:text-white transition-all duration-300'>Register</Link>
             <Link href={'/login'} className='hover:text-2xl hover:text-white transition-all duration-300'>Login</Link>
